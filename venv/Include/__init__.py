860b16cc0369bdbe43540d09ac8607b733b738eb
joao.heckmann <joao.heckmann@senior.com.br>
2019-04-16 16:35:52 -0300

    Adding loop

--- venv/Include/__init__.py
+++ venv/Include/__init__.py
@@ -8,25 +8,27 @@ if (sys.argv[1]):
 else :
     workbookdir = os.getenv('XLS_PATH')
     sheetname = os.getenv('SHEET_NAME')
+
+while(True):
+	# Gets the Workbook URL based on the env variable 'XLS_PATH' or ARGV
+	workbook = xlrd.open_workbook(workbookdir)
+
+	# Gets sheet by name 
+	sheet = workbook.sheet_by_name(sheetname)
+
+	#Get the sheet number of rows and columns
+	num_rows = sheet.nrows
+	print("Number of rows: "+ str(num_rows))
+	num_columns = sheet.ncols
+	print("Number of columns: "+ str(num_columns))
+
+	# Gets the row i need to read
+	current_row = sheet.row(15)
+
+	data = []
+	for i in range(num_columns):
+		data.append(current_row[i].value)
+	print(data)
 	
-# Gets the Workbook URL based on the env variable 'XLS_PATH' or ARGV
-workbook = xlrd.open_workbook(workbookdir)
-
-# Gets sheet by name 
-sheet = workbook.sheet_by_name(sheetname)
-
-#Get the sheet number of rows and columns
-num_rows = sheet.nrows
-print("Number of rows: "+ str(num_rows))
-num_columns = sheet.ncols
-print("Number of columns: "+ str(num_columns))
-
-# Gets the row i need to read
-current_row = sheet.row(15)
-
-data = []
-for i in range(num_columns):
-    data.append(current_row[i].value)
-print(data)
 
 
