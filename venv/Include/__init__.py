6143a160d477d4d53ea02ebcbd5721c9fe3c17d8
joao.heckmann <joao.heckmann@senior.com.br>
2019-04-22 13:32:58 -0300

    Adding sleep time

--- venv/Include/__init__.py
+++ venv/Include/__init__.py
@@ -1,34 +1,33 @@
-import os, sys, xlrd
+import os, sys, xlrd, time
 
 workbookdir = ""
 sheetname = ""
-if (sys.argv[1]):
+if sys.argv[1]:
     workbookdir = sys.argv[1]
     sheetname = sys.argv[2]
-else :
+else:
     workbookdir = os.getenv('XLS_PATH')
     sheetname = os.getenv('SHEET_NAME')
 
-while(True):
-	# Gets the Workbook URL based on the env variable 'XLS_PATH' or ARGV
-	workbook = xlrd.open_workbook(workbookdir)
+while True:
+    # Gets the Workbook URL based on the env variable 'XLS_PATH' or ARGV
+    workbook = xlrd.open_workbook(workbookdir)
 
-	# Gets sheet by name 
-	sheet = workbook.sheet_by_name(sheetname)
+    # Gets sheet by name
+    sheet = workbook.sheet_by_name(sheetname)
 
-	#Get the sheet number of rows and columns
-	num_rows = sheet.nrows
-	print("Number of rows: "+ str(num_rows))
-	num_columns = sheet.ncols
-	print("Number of columns: "+ str(num_columns))
+    # Get the sheet number of rows and columns
+    num_rows = sheet.nrows
+    print("Number of rows: " + str(num_rows))
+    num_columns = sheet.ncols
+    print("Number of columns: " + str(num_columns))
 
-	# Gets the row i need to read
-	current_row = sheet.row(15)
+    # Gets the row i need to read
+    current_row = sheet.row(15)
 
-	data = []
-	for i in range(num_columns):
-		data.append(current_row[i].value)
-	print(data)
-	
+    data = []
+    for i in range(num_columns):
+        data.append(current_row[i].value)
+    print(data)
 
-
+    time.sleep(1)  # Time to sleep
